89881edfbf48f6758b573bcf1ec574d4a4294062
Sergio Vega <hekinky@MacBook-Pro.local>
2021-02-15 17:55:49 -0700

    [Complete Landing Css + Links]

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom' 
+import { Link } from 'react-router-dom'
 import Github from '../assets/Github.png'
 import Linkedin from '../assets/Linkedin.png'
 import Dunes from '../assets/Dunes.jpg'
@@ -9,31 +9,44 @@ import '../css/Landing.css'
 function Landing() {
 
 
-    
-        return (
-            <div className="Yeah" style={{ backgroundImage: `url(${Dunes})`}}>
-                {/* <img className="LandingImageDunes" src={ Dunes }> */}
-                <div className="HeaderWrapper">
-                    <h1 className="HeaderLogo">PIAO ALTO VIEU</h1>
-                    <ul className="LinkSpread">
-                        <li><Link className="LinkAbout" to="/About">ABOUT</Link></li>
-                        <li><Link className="LinkWebsites" to="/Websites">WEBSITES</Link></li>
-                        <li><Link className="LinkDesign" to="/Design">DESIGN</Link></li>
-                        <li><Link className="LinkContact" to="/Contact">CONTACT</Link></li>
-                    </ul>
-                </div>
-                <div className="LandingWrapper">
-                    <div className="LandingUpperBox">
-                        <h1 className="LandingH1Name">SERGIO VEGA</h1>
-                        <h1 className="LandingH1Description">FULL STACK WEB DEVELOPER</h1>
-                    </div>
-                    {/*  */}
-                    <div className="LandingLowerBox"></div>
-                </div>
-                {/* // </img> */}
+
+    return (
+        <div className="Yeah" style={{ backgroundImage: `url(${Dunes})` }}>
+            {/* <img className="LandingImageDunes" src={ Dunes }> */}
+            <div className="HeaderWrapper">
+                <h1 className="HeaderLogo">PIAO ALTO VIEU</h1>
+                <ul className="LinkSpread">
+                    <li><Link className="LinkAbout" to="/About">ABOUT</Link></li>
+                    <li><Link className="LinkWebsites" to="/Websites">WEBSITES</Link></li>
+                    <li><Link className="LinkDesign" to="/Design">DESIGN</Link></li>
+                    <li><Link className="LinkContact" to="/Contact">CONTACT</Link></li>
+                </ul>
             </div>
-        )
-    
+            <div className="LandingWrapper">
+                <div className="LandingUpperBox">
+                    <h1 className="LandingH1Name">SERGIO VEGA</h1>
+                    <h1 className="LandingH1Description">FULL STACK WEB DEVELOPER</h1>
+                </div>
+                {/*  */}
+                <div className="LandingLowerBox"></div>
+            </div>
+            <div className="IconWrapper">
+                <div className="Icons">
+                </div>
+                <div className="Icon2">
+                    <Link to="/https://github.com/blankvs">
+                        <img className="GitIcon" src={Github}></img>
+                    </Link>
+                    <Link to="/https://www.linkedin.com/in/sergio-vega-902a151b9/">
+                        <img className="LinkedinIcon" src={Linkedin}></img>
+                    </Link>
+                </div>
+            </div>
+            <div className="LeftLineLanding"></div>
+            {/* // </img> */}
+        </div>
+    )
+
 }
 
 export default Landing;
