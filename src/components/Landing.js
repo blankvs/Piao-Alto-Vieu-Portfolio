748826f2b494b9b2efcbc03b06acaf752bc50a24
Sergio Vega <hekinky@MacBook-Pro.local>
2021-03-13 03:36:44 -0700

    [In Progress Phone]

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -15,62 +15,94 @@ function Landing() {
 
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-    function toggleMenu() {
-        setIsMenuOpen(!isMenuOpen)
+    // function toggleMenu() {
+    //     setIsMenuOpen(!isMenuOpen)
+    // }
+
+    let menu
+
+    if (isMenuOpen) {
+        menu =
+            <div className="fixBox">
+                <div className="mobileBox">
+                    <div className="yerr">
+                        <img className="ThreeMenu"
+                            src={Three}
+                            onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        />
+                    </div>
+                    <p><Link className="LinkWebsitesMob" to="/">HOME</Link></p>
+                    <p><Link className="LinkAboutMob" to="/About">ABOUT</Link></p>
+                    <p><Link className="LinkWebsitesMob" to="/Websites">WEBSITES</Link></p>
+                    <p><Link className="LinkDesignMob" to="/Design">DESIGN</Link></p>
+                    <p><Link className="LinkContactMob" to="/Contact">CONTACT</Link></p>
+                </div>
+            </div>
     }
 
     return (
-        <div className="Yeah" style={{ backgroundImage: `url(${Landing2})` }} >
-            <div className="HeaderWrapper">
-                <h1 className="HeaderLogo">PIAO ALTO VIEU</h1>
-                <ul className="LinkSpread">
-                    <li><Link className="LinkWebsites" to="/">HOME</Link></li>
-                    <li><Link className="LinkAbout" to="/About">ABOUT</Link></li>
-                    <li><Link className="LinkWebsites" to="/Websites">WEBSITES</Link></li>
-                    <li><Link className="LinkDesign" to="/Design">DESIGN</Link></li>
-                    <li><Link className="LinkContact" to="/Contact">CONTACT</Link></li>
-                </ul>
+        <div>
+            {menu}
+            <div className="Yeah" style={{ backgroundImage: `url(${Landing2})` }} >
+                <div className="HeaderWrapper">
+                    <h1 className="HeaderLogo">PIAO ALTO VIEU</h1>
+                    <div className="move">
+                        <ul className="LinkSpread">
+                            <li><Link className="LinkWebsites" to="/">HOME</Link></li>
+                            <li><Link className="LinkAbout" to="/About">ABOUT</Link></li>
+                            <li><Link className="LinkWebsites" to="/Websites">WEBSITES</Link></li>
+                            <li><Link className="LinkDesign" to="/Design">DESIGN</Link></li>
+                            <li><Link className="LinkContact" to="/Contact">CONTACT</Link></li>
+                        </ul>
+                    </div>
 
-                <img className="ThreeMenu"
-                    onClick={toggleMenu}
-                    src={Three}
-                />
-
-                <div className={`mobileNav ${isMenuOpen ? null : "mobileNavHide"}`}>
-                    <ul className="LinkSpread">
-                        <li><Link className="LinkWebsitesMob" to="/">HOME</Link></li>
-                        <li><Link className="LinkAboutMob" to="/About">ABOUT</Link></li>
-                        <li><Link className="LinkWebsitesMob" to="/Websites">WEBSITES</Link></li>
-                        <li><Link className="LinkDesignMob" to="/Design">DESIGN</Link></li>
-                        <li><Link className="LinkContactMob" to="/Contact">CONTACT</Link></li>
-                    </ul>
+                    <div className="yerr">
+                        <img className="ThreeMenu"
+                            src={Three}
+                            onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        />
+                    </div>
                 </div>
 
-            </div>
-            <div className="LandingWrapper">
-                <div className="LandingUpperBox">
-                    <h1 className="LandingH1Name">SERGIO VEGA</h1>
-                    <h1 className="LandingH1Description">FULL STACK WEB DEVELOPER / DIGITAL MEDIA SPECIALIST</h1>
+                {/* <div className={`mobileNav ${isMenuOpen ? null : "mobileNavHide"}`}>
+                    <div className="mobileBox">
+                        <p><Link className="LinkWebsitesMob" to="/">HOME</Link></p>
+                        <p><Link className="LinkAboutMob" to="/About">ABOUT</Link></p>
+                        <p><Link className="LinkWebsitesMob" to="/Websites">WEBSITES</Link></p>
+                        <p><Link className="LinkDesignMob" to="/Design">DESIGN</Link></p>
+                        <p><Link className="LinkContactMob" to="/Contact">CONTACT</Link></p>
+                    </div>
+                </div> */}
+
+
+                <div className="LandingWrapper">
+                    <div className="LandingUpperBox">
+                        <h1 className="LandingH1Name">SERGIO VEGA</h1>
+                        <h1 className="LandingH1Description">FULL STACK WEB DEVELOPER / DIGITAL MEDIA SPECIALIST</h1>
+                    </div>
+                    {/*  */}
+                    <div className="LandingLowerBox"></div>
                 </div>
-                {/*  */}
-                <div className="LandingLowerBox"></div>
-            </div>
-            <div className="IconWrapper">
-                <div className="Icons">
+                <div className="IconWrapper">
+                    <div className="Icons">
+                    </div>
+                    <div className="Icon2">
+                        <Link to="/https://github.com/blankvs">
+                            <img className="GitIcon" src={Github}></img>
+                        </Link>
+                        <Link to="/https://www.linkedin.com/in/sergio-vega-902a151b9/">
+                            <img className="LinkedinIcon" src={Linkedin}></img>
+                        </Link>
+                    </div>
                 </div>
-                <div className="Icon2">
-                    <Link to="/https://github.com/blankvs">
-                        <img className="GitIcon" src={Github}></img>
-                    </Link>
-                    <Link to="/https://www.linkedin.com/in/sergio-vega-902a151b9/">
-                        <img className="LinkedinIcon" src={Linkedin}></img>
-                    </Link>
+                <div className="LeftLineLanding">
+                    <img className="Skills" src={Skills}></img>
                 </div>
+                <h1 className="cc">
+                    © 2021 PiaoAltoVieu.
+                </h1>
+                {/* // </img> */}
             </div>
-            <div className="LeftLineLanding">
-                <img className="Skills" src={Skills}></img>
-            </div>
-            {/* // </img> */}
         </div>
     )
 
