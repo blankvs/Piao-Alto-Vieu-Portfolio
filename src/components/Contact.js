38ef5c9273f0c2117f5bd6d9ce12fd8e4bf7a264
Sergio Vega <hekinky@MacBook-Pro.local>
2021-03-14 13:49:03 -0600

    [Media Complete]

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import WATER from '../assets/WATER.png'
+import Three from '../assets/Three.png'
 import '../css/Contact.css'
 
 function Contact() {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    let menu
+
+    if (isMenuOpen) {
+        menu =
+            <div className="fixBox">
+                <div className="mobileBox">
+                    <p><Link className="LinkWebsitesMob" to="/">HOME</Link></p>
+                    <p><Link className="LinkWebsitesMob" to="/About">ABOUT</Link></p>
+                    <p><Link className="LinkWebsitesMob" to="/Websites">WEBSITES</Link></p>
+                    <p><Link className="LinkWebsitesMob" to="/Design">DESIGN</Link></p>
+                    <p><Link className="LinkWebsitesMob" to="/Contact">CONTACT</Link></p>
+                </div>
+            </div>
+    }
 
     return (
         <div className="Yeah" style={{ backgroundImage: `url(${WATER})` }} >
@@ -12,6 +30,12 @@ function Contact() {
             <div>
                 <div className="HeaderWrapper">
                     <h1 className="HeaderLogo">PIAO ALTO VIEU</h1>
+                    <div className="yerr">
+                        <img className="ThreeMenu"
+                            src={Three}
+                            onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        />
+                    </div>
                     <ul className="LinkSpread">
                         <li><Link className="LinkWebsites" to="/">HOME</Link></li>
                         <li><Link className="LinkAbout" to="/About">ABOUT</Link></li>
@@ -21,9 +45,14 @@ function Contact() {
                     </ul>
                 </div>
             </div>
+            {menu}
             <h1 className="contactH1">CONTACT</h1>
             <h1 className="emailH1">SERGIOVEGAP12@GMAIL.COM</h1>
-            
+            <div className="footer">
+                <h1 className="cc">
+                    © 2021 PiaoAltoVieu.
+                </h1>
+            </div>
         </div>
     )
 }
--- src/components/Design.js
+++ src/components/Design.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import DESIFOR from '../assets/DESIFOR.png'
+import Three from '../assets/Three.png'
 import '../css/Design.css'
 
 function Design() {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    let menu
+
+    if (isMenuOpen) {
+        menu =
+            <div className="fixBox">
+                <div className="mobileBox">
+                    <p><Link className="LinkWebsitesMob" to="/">HOME</Link></p>
+                    <p><Link className="LinkWebsitesMob" to="/About">ABOUT</Link></p>
+                    <p><Link className="LinkWebsitesMob" to="/Websites">WEBSITES</Link></p>
+                    <p><Link className="LinkWebsitesMob" to="/Design">DESIGN</Link></p>
+                    <p><Link className="LinkWebsitesMob" to="/Contact">CONTACT</Link></p>
+                </div>
+            </div>
+    }
 
     return (
         <div className="Yeah" style={{ backgroundImage: `url(${DESIFOR})` }} >
@@ -12,6 +30,12 @@ function Design() {
             <div>
                 <div className="HeaderWrapper">
                     <h1 className="HeaderLogo">PIAO ALTO VIEU</h1>
+                    <div className="yerr">
+                        <img className="ThreeMenu"
+                            src={Three}
+                            onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        />
+                    </div>
                     <ul className="LinkSpread">
                         <li><Link className="LinkWebsites" to="/">HOME</Link></li>
                         <li><Link className="LinkAbout" to="/About">ABOUT</Link></li>
@@ -21,8 +45,15 @@ function Design() {
                     </ul>
                 </div>
             </div>
+            {menu}
             <h1 className="designH1">DESIGN</h1>
-            
+
+            <div className="footer">
+                <h1 className="cc">
+                    © 2021 PiaoAltoVieu.
+                </h1>
+            </div>
+
         </div>
     )
 }
